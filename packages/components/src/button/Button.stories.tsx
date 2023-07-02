780d1b573e450df60e5645521a3254b356a0b585
sor4chi <sorakawa0218@gmail.com>
2023-07-03 08:00:49 +0900

    feat: buttonコンポーネントの基礎の作成

--- packages/components/src/button/Button.stories.tsx
+++ packages/components/src/button/Button.stories.tsx
@@ -1,16 +1,53 @@
+import { HStack } from '../__stories__/h-stack';
+import { VStack } from '../__stories__/v-stack';
+
 import { Button } from './Button';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'tertiary'],
+    },
+    disabled: { control: { type: 'boolean' } },
+    onClick: { action: 'clicked' },
+  },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Button>;
 
-export const Default: Story = {
-  args: {
-    children: 'Button',
-  },
-};
+const Buttons = (args: Story['args']) => (
+  <HStack>
+    <VStack theme="light">
+      <Button variant="primary" {...args}>
+        Primary
+      </Button>
+      <Button variant="secondary" {...args}>
+        Secondary
+      </Button>
+      <Button variant="tertiary" {...args}>
+        Tertiary
+      </Button>
+    </VStack>
+    <VStack theme="dark">
+      <Button variant="primary" {...args}>
+        Primary
+      </Button>
+      <Button variant="secondary" {...args}>
+        Secondary
+      </Button>
+      <Button variant="tertiary" {...args}>
+        Tertiary
+      </Button>
+    </VStack>
+  </HStack>
+);
+
+export const Default = (args: Story['args']) => <Buttons {...args} />;
+
+export const Disabled = (args: Story['args']) => <Buttons {...args} disabled />;
--- packages/components/src/button/Button.tsx
+++ packages/components/src/button/Button.tsx
@@ -1,17 +1,31 @@
+import { clsx } from 'clsx';
 import { forwardRef, memo } from 'react';
+
+import styles from './Button.module.scss';
 
 import type { ReactNode, Ref, ComponentPropsWithoutRef } from 'react';
 
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   children: ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 }
 
 const _Button = (
-  { children, ...rect }: ButtonProps,
+  { children, className, variant = 'primary', ...rect }: ButtonProps,
   ref: Ref<HTMLButtonElement>,
 ) => {
   return (
-    <button ref={ref} {...rect}>
+    <button
+      ref={ref}
+      {...rect}
+      className={clsx(
+        styles.button,
+        className,
+        variant === 'primary' && styles.primary,
+        variant === 'secondary' && styles.secondary,
+        variant === 'tertiary' && styles.tertiary,
+      )}
+    >
       {children}
     </button>
   );
