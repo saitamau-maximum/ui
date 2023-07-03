f6f1a0a54615f27eaa06c8f5cf0f77ed6e951d2e
sor4chi <sorakawa0218@gmail.com>
2023-07-03 10:28:56 +0900

    feat: buttonにiconを設定できるよう追加

--- packages/components/src/button/Button.stories.tsx
+++ packages/components/src/button/Button.stories.tsx
@@ -1,3 +1,5 @@
+import { ArrowRight, ArrowLeft } from 'react-feather';
+
 import { HStack } from '../__stories__/h-stack';
 import { VStack } from '../__stories__/v-stack';
 
@@ -51,3 +53,34 @@ const Buttons = (args: Story['args']) => (
 export const Default = (args: Story['args']) => <Buttons {...args} />;
 
 export const Disabled = (args: Story['args']) => <Buttons {...args} disabled />;
+
+export const WithIcons = (args: Story['args']) => (
+  <Buttons
+    {...args}
+    leftIcon={<ArrowLeft size="1.5em" />}
+    rightIcon={<ArrowRight size="1.5em" />}
+  />
+);
+
+export const IconOnly = (args: Story['args']) => (
+  <HStack>
+    <VStack theme="light">
+      <Button variant="primary" icon={<ArrowRight size="1.5em" />} {...args} />
+      <Button
+        variant="secondary"
+        icon={<ArrowRight size="1.5em" />}
+        {...args}
+      />
+      <Button variant="tertiary" icon={<ArrowRight size="1.5em" />} {...args} />
+    </VStack>
+    <VStack theme="dark">
+      <Button variant="primary" icon={<ArrowRight size="1.5em" />} {...args} />
+      <Button
+        variant="secondary"
+        icon={<ArrowRight size="1.5em" />}
+        {...args}
+      />
+      <Button variant="tertiary" icon={<ArrowRight size="1.5em" />} {...args} />
+    </VStack>
+  </HStack>
+);
--- packages/components/src/button/Button.tsx
+++ packages/components/src/button/Button.tsx
@@ -6,14 +6,28 @@ import styles from './Button.module.scss';
 import type { ReactNode, Ref, ComponentPropsWithoutRef } from 'react';
 
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
-  children: ReactNode;
+  children?: ReactNode;
   variant?: 'primary' | 'secondary' | 'tertiary';
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  // iconはrightIconのエイリアス
+  icon?: ReactNode;
 }
 
 const _Button = (
-  { children, className, variant = 'primary', ...rect }: ButtonProps,
+  {
+    children,
+    className,
+    variant = 'primary',
+    leftIcon,
+    rightIcon,
+    icon,
+    ...rect
+  }: ButtonProps,
   ref: Ref<HTMLButtonElement>,
 ) => {
+  const noText = !children;
+
   return (
     <button
       ref={ref}
@@ -24,9 +38,12 @@ const _Button = (
         variant === 'primary' && styles.primary,
         variant === 'secondary' && styles.secondary,
         variant === 'tertiary' && styles.tertiary,
+        noText ? styles.iconOnly : styles.withText,
       )}
     >
+      {leftIcon}
       {children}
+      {rightIcon || icon}
     </button>
   );
 };
