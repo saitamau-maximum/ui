bbd169b057985269303e901c448527bb5ed91ea0
sor4chi <sorakawa0218@gmail.com>
2023-08-30 12:48:43 +0900

    feat: story utility componentにcontainerを追加

--- packages/components/src/timeline/Timeline.stories.tsx
+++ packages/components/src/timeline/Timeline.stories.tsx
@@ -1,3 +1,4 @@
+import { Container } from '../__stories__/containter';
 import { HStack } from '../__stories__/h-stack';
 import { VStack } from '../__stories__/v-stack';
 
@@ -46,4 +47,41 @@ export const Default = () => (
       </Timeline.Container>
     </VStack>
   </HStack>
+);
+
+export const Expand = () => (
+  <>
+    <Container theme="light">
+      <Timeline.Container expand>
+        <Timeline.Item label="2021-01-01" expand>
+          競プロのコンテスト○○を開催しました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-02" active expand>
+          ××に参加し、3位入賞を果たしました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-03" expand>
+          △△をこちらにて公開しました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-04" active expand>
+          ◻︎◻︎を新たにリニューアルしました
+        </Timeline.Item>
+      </Timeline.Container>
+    </Container>
+    <Container theme="dark">
+      <Timeline.Container>
+        <Timeline.Item label="2021-01-01" active expand>
+          競プロのコンテスト○○を開催しました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-02" expand>
+          ××に参加し、3位入賞を果たしました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-03" active expand>
+          △△をこちらにて公開しました
+        </Timeline.Item>
+        <Timeline.Item label="2021-01-04" expand>
+          ◻︎◻︎を新たにリニューアルしました
+        </Timeline.Item>
+      </Timeline.Container>
+    </Container>
+  </>
 );
